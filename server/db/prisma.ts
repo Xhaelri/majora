0348a1503d534e52bbfc71e98c29bb09a0b56070
Ali Saleh <156487348+Xhaelri@users.noreply.github.com>
2025-07-07 23:59:53 +0300

    fix

--- server/db/prisma.ts
+++ server/db/prisma.ts
@@ -146,3 +146,11 @@ export async function getCategoryBySlug(slug: string) {
     }
   });
 }
+
+import { neon } from "@neondatabase/serverless";
+
+export async function getData() {
+    const sql = neon(process.env.DATABASE_URL!);
+    const data = await sql`...`;
+    return data;
+}
