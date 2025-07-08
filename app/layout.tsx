5677b04f2982a3166efa5e878acdb0af6be2aeaa
Ali Saleh <156487348+Xhaelri@users.noreply.github.com>
2025-07-08 03:02:10 +0300

    without herader- with best-seller

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default async function RootLayout({
         <SessionProvider session={session}>
           <CartProvider>
               <ThemeContextProvider>
-                {/* <Header /> */}
+                {/* <Header /> */} 
                 <Toaster
                   position="top-right"
                   expand={false}
