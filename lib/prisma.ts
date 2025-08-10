5b0a29e41956f892a0dca723da5983137a2c6dec
Ali Saleh <ali.saleh.ali172@gmail.com>
2025-08-10 13:51:23 +0300

    neon

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,14 @@
-import { Environments } from "@/constants/constants";
-import { PrismaClient } from "./generated/prisma";
+// @/lib/prisma.ts
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
 export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log:
-      process.env.NODE_ENV === Environments.DEV
-        ? ["query", "error", "warn"]
-        : ["error"],
-  });
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  })
 
-  if (process.env.NODE_ENV !== Environments.PROD) globalForPrisma.prisma = db
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
